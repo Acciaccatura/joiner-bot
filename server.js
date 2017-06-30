809494dcef39c0489f71e2db29a00700eb92a27d
Chia <wchia@aus2m-wchia.rws.ad.ea.com>
2017-06-30 15:07:32 -0500

    first commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const SLACK_APP_TOKEN = process.env.SLACK_APP_TOKEN
 const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN
+const SLACK_BOT_USER_ID = process.env.SLACK_BOT_USER_ID
 
 const express = require('express')
 const Slapp = require('slapp')
@@ -11,7 +12,8 @@ var port = process.env.PORT || 3000
 
 var contextData = {
   app_token: SLACK_APP_TOKEN,
-  bot_token: SLACK_BOT_TOKEN
+  bot_token: SLACK_BOT_TOKEN,
+  bot_user_id: SLACK_BOT_USER_ID
 }
 
 console.log(contextData)
